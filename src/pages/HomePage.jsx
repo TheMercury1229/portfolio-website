ccdf5c02f1f77d1b163c9dcb2cb5dc2a325d82d7
Hardik Gujrathi <gujarathihardik29@gmail.com>
2025-07-18 23:44:49 +0530

    Refactor import statements and improve formatting in HomePage and Contact components

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,49 +1,48 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 import { useGSAP } from "@gsap/react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-import Header from '../components/Header';
-import Hero from '../components/Hero';
-import About from '../components/About';
-import Skill from '../components/Skill';
-import Work from '../components/Work';
-import Contact from '../components/Contact';
-import Footer from '../components/Footer';
-import ReactLenis from 'lenis/react';
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import About from "../components/About";
+import Skill from "../components/Skill";
+import Work from "../components/Work";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
+import ReactLenis from "lenis/react";
 gsap.registerPlugin(ScrollTrigger, useGSAP);
 
 const HomePage = () => {
-  useEffect(()=>{
+  useEffect(() => {
     window.scrollTo(0, 0);
     gsap.fromTo(
-        ".section-2",
-        { opacity: 0 },
-        { opacity: 1, duration: 3, ease: "power2.out" }
-      );
-    
-  },[])
-    useGSAP(() => {
-      const elements = gsap.utils.toArray(".reveal-up");
-      elements.forEach((element) => {
-        gsap.to(element, {
-          scrollTrigger: {
-            trigger: element,
-            start: "-100 bottom",
-            end: "bottom 100%",
-            scrub: true,
-          },
-          y: 0,
-          opacity: 1,
-          duration: 1,
-          ease: "power2.out",
-        });
+      ".section-2",
+      { opacity: 0 },
+      { opacity: 1, duration: 3, ease: "power2.out" }
+    );
+  }, []);
+  useGSAP(() => {
+    const elements = gsap.utils.toArray(".reveal-up");
+    elements.forEach((element) => {
+      gsap.to(element, {
+        scrollTrigger: {
+          trigger: element,
+          start: "-100 bottom",
+          end: "bottom 100%",
+          scrub: true,
+        },
+        y: 0,
+        opacity: 1,
+        duration: 1,
+        ease: "power2.out",
       });
     });
+  });
   return (
     <ReactLenis root>
       <Header />
-      <main className='section-2'>
+      <main className="section-2">
         <Hero />
         <About />
         <Skill />
@@ -53,6 +52,6 @@ const HomePage = () => {
       <Footer />
     </ReactLenis>
   );
-}
+};
 
-export default HomePage
+export default HomePage;
